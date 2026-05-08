b18692fb594e2701a9a985aa15a26a8d445a3ff3
Abrahim Mejía Valdez <abrmeval@outlook.com>
2026-05-08 00:44:00 -0600

    fix: piepline and docusaurs config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://abrmeval.github.io',
+  url: 'https://dev-guidelines-przk9sayy-abrmevals-projects.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/dev-guidelines/',
