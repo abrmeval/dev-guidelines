c9153d847c8d0443c12cc85e0749a0b2793934df
Abrahim Mejía Valdez <abrmeval@outlook.com>
2026-09-09 15:27:35 -0600

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://dev-guidelines-przk9sayy-abrmevals-projects.vercel.app',
+  url: 'https://project-o16o8.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
